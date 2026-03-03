e0ac9e1c3a2e4409c1e4cc7638b6cf366b968e72
gorgelpinto <84418613+gorgelpinto@users.noreply.github.com>
2026-03-03 16:14:15 +0000

    sketch.js

--- sketch.js
+++ sketch.js
@@ -1,34 +1,110 @@
-display() {
-  if (!this.isActive) return;
-  if (this.spacing <= 0) return;
+let layers = [];
+let images = {};
 
-  // Encontrar ponto inicial alinhado à grelha
-  let startX = -((this.horizontalOffset % this.spacing) + this.spacing);
-  let startY = -((this.verticalOffset % this.spacing) + this.spacing);
+function preload() {
+  images[1] = loadImage("img1.png");
+  images[2] = loadImage("img2.png");
+}
 
-  for (let x = startX; x < width + this.spacing; x += this.spacing) {
-    for (let y = startY; y < height + this.spacing; y += this.spacing) {
+function setup() {
+  const container = document.getElementById("sketch-container");
 
-      push();
+  const canvas = createCanvas(
+    container.offsetWidth,
+    container.offsetHeight
+  );
 
-      translate(
-        x + this.horizontalOffset,
-        y + this.verticalOffset
-      );
+  canvas.parent("sketch-container");
 
-      rotate(this.rotationAngle);
-      tint(255, this.transparency);
-      imageMode(CENTER);
+  layers.push(new Layer(1, "l1"));
+  layers.push(new Layer(2, "l2"));
 
-      image(
-        this.image,
-        0,
-        0,
-        this.imageSize,
-        this.imageSize
-      );
+  noLoop();
+}
 
-      pop();
+function draw() {
+  background(255);
+
+  layers.forEach(layer => {
+    layer.update();
+    layer.display();
+  });
+}
+
+function windowResized() {
+  const container = document.getElementById("sketch-container");
+
+  resizeCanvas(
+    container.offsetWidth,
+    container.offsetHeight
+  );
+
+  redraw();
+}
+
+class Layer {
+  constructor(index, prefix) {
+    this.image = images[index];
+
+    this.enabled = document.getElementById(prefix + "-enabled");
+
+    this.controls = {
+      transparency: document.getElementById(prefix + "-transparency"),
+      rotation: document.getElementById(prefix + "-rotation"),
+      spacing: document.getElementById(prefix + "-spacing"),
+      size: document.getElementById(prefix + "-size"),
+      x: document.getElementById(prefix + "-x"),
+      y: document.getElementById(prefix + "-y"),
+    };
+
+    this.enabled.addEventListener("change", () => redraw());
+
+    Object.values(this.controls).forEach(control => {
+      control.addEventListener("input", () => redraw());
+    });
+  }
+
+  update() {
+    this.isActive = this.enabled.checked;
+
+    this.rotationAngle = radians(Number(this.controls.rotation.value));
+    this.spacing = Number(this.controls.spacing.value);
+    this.imageSize = Number(this.controls.size.value);
+    this.transparency = Number(this.controls.transparency.value);
+    this.horizontalOffset = Number(this.controls.x.value);
+    this.verticalOffset = Number(this.controls.y.value);
+  }
+
+  display() {
+    if (!this.isActive) return;
+    if (this.spacing <= 0) return;
+
+    const buffer = this.spacing * 2;
+
+    for (let x = -buffer; x < width + buffer; x += this.spacing) {
+      for (let y = -buffer; y < height + buffer; y += this.spacing) {
+
+        push();
+
+        translate(
+          x + this.horizontalOffset,
+          y + this.verticalOffset
+        );
+
+        rotate(this.rotationAngle);
+        tint(255, this.transparency);
+        imageMode(CENTER);
+
+        image(
+          this.image,
+          0,
+          0,
+          this.imageSize,
+          this.imageSize
+        );
+
+        pop();
+      }
     }
   }
 }
